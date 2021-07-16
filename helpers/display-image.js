a64efce79e2b334e8e443e7a99a426bb42184a4a
Weaver Goldman <we.goldm@gmail.com>
2021-07-16 15:02:15 -0400

    Added the ability to apply the transforms to another image.

--- helpers/display-image.js
+++ helpers/display-image.js
@@ -48,6 +48,96 @@ export const displayImage = ({
 	return plot
 }
 
+export const replaceCropInSourceImage = async ({
+	imageData,
+	width,
+	height,
+	canvasElement,
+	imageRange = [0, 255],
+	colorScale = "greys",
+	cropPosition,
+	cropDimensions,
+	angle = null,
+	newCrop,
+}) => {
+	const ctx = canvasElement.getContext("2d")
+
+	const { x, y } = cropPosition
+	const { width: cropWidth, height: cropHeight } = cropDimensions
+
+	// Create a plotty plot for the given image
+	const plot = new plotty.plot({
+		canvas: canvasElement,
+		data: imageData,
+		width,
+		height,
+		domain: imageRange,
+		colorScale,
+		useWebGL: false,
+	})
+
+	// Render the plot to the canvas
+	plot.render()
+
+	// Convert the new crop to a canvas image
+	const tempCanvas = document.createElement("canvas")
+
+	const tempPlot = new plotty.plot({
+		canvas: tempCanvas,
+		data: newCrop.data[0].data,
+		width: newCrop.dimensions.width,
+		height: newCrop.dimensions.height,
+		domain: imageRange,
+		colorScale,
+		useWebGL: false,
+	})
+
+	// Render the plot to the canvas
+	tempPlot.render()
+
+	const tempCanvasContext = tempCanvas.getContext("2d")
+
+	const newCropImage = await createImageBitmap(
+		tempCanvasContext.getImageData(
+			0,
+			0,
+			newCrop.dimensions.width,
+			newCrop.dimensions.height
+		)
+	)
+
+	ctx.save()
+
+	// Apply transformations
+	ctx.translate(x + cropWidth / 2, y + cropHeight / 2)
+	if (angle != null) ctx.rotate(radians(angle))
+
+	ctx.drawImage(newCropImage, -cropWidth / 2, -cropHeight / 2)
+
+	// Reset the transformation matrix
+	ctx.restore()
+
+	if (angle != null) {
+		ctx.save()
+
+		const image = await createImageBitmap(
+			ctx.getImageData(x, y, cropWidth, cropHeight)
+		)
+
+		// Apply transformations
+		ctx.translate(x + cropWidth / 2, y + cropHeight / 2)
+
+		ctx.drawImage(image, -cropWidth / 2, -cropHeight / 2)
+
+		// Reset the transformation matrix
+		ctx.restore()
+	}
+
+	document.querySelector("#replaced-crop-canvas").style.visibility = "visible"
+
+	return plot
+}
+
 export const showCroppedAreaOnImage = async ({
 	imageData,
 	width,
@@ -64,10 +154,7 @@ export const showCroppedAreaOnImage = async ({
 	const { x, y } = cropPosition
 	const { width: cropWidth, height: cropHeight } = cropDimensions
 
-	const [canvasWidth, canvasHeight] = [
-		width,
-		height,
-	]
+	const [canvasWidth, canvasHeight] = [width, height]
 
 	// Create a plotty plot for the given image
 	const plot = new plotty.plot({
@@ -82,29 +169,33 @@ export const showCroppedAreaOnImage = async ({
 
 	// Render the plot to the canvas
 	plot.render()
-	
+
 	// Make every other area of the canvas black
 	ctx.fillStyle = "rgb(0,0,0)"
 
 	ctx.save()
 
-	const image = await createImageBitmap(ctx.getImageData(x, y, cropWidth, cropHeight))
+	const image = await createImageBitmap(
+		ctx.getImageData(x, y, cropWidth, cropHeight)
+	)
 
 	ctx.fillRect(0, 0, canvasWidth, canvasHeight)
 
 	// Apply transformations
 	ctx.translate(x + cropWidth / 2, y + cropHeight / 2)
-	if(angle != null) ctx.rotate(radians(angle))
+	if (angle != null) ctx.rotate(radians(angle))
 
 	ctx.drawImage(image, -cropWidth / 2, -cropHeight / 2)
 
 	// Reset the transformation matrix
 	ctx.restore()
 
-	if(angle != null) {
+	if (angle != null) {
 		ctx.save()
 
-		const image = await createImageBitmap(ctx.getImageData(x, y, cropWidth, cropHeight))
+		const image = await createImageBitmap(
+			ctx.getImageData(x, y, cropWidth, cropHeight)
+		)
 
 		ctx.fillRect(0, 0, canvasWidth, canvasHeight)
 
--- main.js
+++ main.js
@@ -11,6 +11,7 @@ import {
 	displayImage,
 	overlayCrop,
 	showCroppedAreaOnImage,
+	replaceCropInSourceImage,
 } from "./helpers/display-image"
 import { isImageRotated } from "./helpers/detect-rotation"
 import {
@@ -43,6 +44,39 @@ const cropImageUploadHandler = createImageUploadCallback({
 		analyzeImages()
 	},
 })
+
+const replaceCropUploadHandler = createImageUploadCallback({
+	callback: (image) => {
+		if (images.crop == null || images.crop.position == null) return
+		if (
+			images.crop.dimensions.width != image.dimensions.width ||
+			images.crop.dimensions.height != image.dimensions.height
+		)
+			return
+
+		const replacedCropCanvas = document.querySelector(
+			"#replaced-crop-canvas"
+		)
+
+		const sourceImageLayer =
+			images.sourceImage.data[images.crop.position.z - 1]
+
+		replaceCropInSourceImage({
+			imageData: sourceImageLayer.data,
+			width: sourceImageLayer.imageWidth,
+			height: sourceImageLayer.imageLength,
+			canvasElement: replacedCropCanvas,
+			cropPosition: images.crop.position,
+			cropDimensions: images.crop.dimensions,
+			angle: images.crop.isRotated ? images.crop.angle : null,
+			newCrop: image,
+		})
+	},
+})
+
+document
+	.querySelector("#new-crop-input")
+	.addEventListener("change", replaceCropUploadHandler)
 
 document
 	.querySelector("#source-image-input")
@@ -151,6 +185,8 @@ const pipelineLayerCompleteCallback = ({ layers, layerFinished }) => {
  */
 const analyzeImages = async () => {
 	if (images.sourceImage == null || images.crop == null) return
+
+	document.querySelector("#replaced-crop-canvas").style.visibility = "hidden"
 
 	document.querySelector("#info-area").style.visibility = "visible"
 
